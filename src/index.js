445fcfc33ee65a9058897a60ef95a9126a45b911
Infant Frontender <newbornfrontender@gmail.com>
2019-01-09 16:41:36 +0300

    Add sample from "Classes, Properties and Methods"

---
+++ src/index.js
@@ -0,0 +1,17 @@
+class Human {
+  gender = 'male';
+
+  printGender = () => console.log(this.gender);
+}
+
+class Person extends Human {
+  name = 'Ann';
+  gender = 'female';
+
+  printName = () => console.log(this.name);
+}
+
+const person = new Person();
+
+person.printName();
+person.printGender();
